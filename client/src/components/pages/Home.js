402f8353b4acc8f7d34cfb7c5f808403ba3a3aa4
Paul Aglipay <paglipay@gmail.com>
2021-01-12 20:51:31 -0800

    basic template

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -35,46 +35,46 @@ function Home() {
                     </p>
                 </div>
             </Jumbotron>
-            <section className="section section-icons grey lighten-4 center" style={{ backgroundColor: '#f4f4f4',height:'100px' }}>
+            <section className="section section-icons grey lighten-4 center" style={{ backgroundColor: '#f4f4f4', height: '100px' }}>
 
             </section>
-            <section className="section section-icons white lighten-4 center" style={{height:'100px'}}>
+            <section className="section section-icons white lighten-4 center" style={{ height: '100px' }}>
 
             </section>
-            <section className="section section-icons grey lighten-4 center" style={{ backgroundColor: '#f4f4f4', height:'100px' }}>
+            <section className="section section-icons grey lighten-4 center" style={{ backgroundColor: '#f4f4f4', height: '100px' }}>
 
-            </section>      
+            </section>
             <section>
-        <Container>
-          <Row>
-            <Col>
-              <h1>Featured Project</h1>
-              <p>Cisco Configuration Parser - Skilled Software Developer with experience in Network / Systems Engineering. Experienced Programmer Analyst / Network Engineer with a demonstrated history of working in the higher education industry. Proficient in Full-Stack development particularly in the MERN stack. </p>
-              <Button variant="flat">Learn more</Button>
-            </Col>
-            <Col>
-              <h1>Home Page</h1>
+                <Container>
+                    <Row>
+                        <Col lg={4}>
+                            <h1>Featured Project</h1>
+                            <p>Cisco Configuration Parser - Skilled Software Developer with experience in Network / Systems Engineering. Experienced Programmer Analyst / Network Engineer with a demonstrated history of working in the higher education industry. Proficient in Full-Stack development particularly in the MERN stack. </p>
+                            <Button variant="flat">Learn more</Button>
+                        </Col>
+                        <Col lg={4}>
+                            <h1>Home Page</h1>
 
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
               varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
               Etiam ut feugiat ex. Cras non risus mi.
 
-          </Col>
-            <Col>
-              <h1>Home Page</h1>
-              <p>
-                Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-                imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-                ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-                elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-                consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-                malesuada fames ac ante ipsum primis in faucibus.
+          </Col >
+                        <Col lg={4}>
+                            <h1>Home Page</h1>
+                            <p>
+                                Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+                                imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
+                                ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
+                                elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+                                consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
+                                malesuada fames ac ante ipsum primis in faucibus.
             </p>
-            </Col>
-          </Row>
+                        </Col>
+                    </Row>
 
-        </Container>
-      </section>
+                </Container>
+            </section>
 
         </div>
     );
